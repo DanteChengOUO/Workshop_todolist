7e7d527808933a8bb4ae5186562e4746c0e78104
dante <dantecherng@hotmail.com.tw>
2020-12-08 14:55:02 +0800

    delete_list

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,11 +1,21 @@
 // TO DO
 document.addEventListener('DOMContentLoaded', function() {
     const checkedlist = document.querySelectorAll('li')
-        // console.log(checkedlist)
     for (let i = 0; i < checkedlist.length; i++) {
         checkedlist[i].addEventListener('click', function() {
             checkedlist[i].classList.add('checked')
         })
     }
+
+
+    const closelist = document.querySelectorAll('li span')
+    for (let n = 0; n < closelist.length; n++) {
+        closelist[n].addEventListener('click', function() {
+            closelist[n].parentElement.remove()
+        })
+    }
+
+
+
 
 })
