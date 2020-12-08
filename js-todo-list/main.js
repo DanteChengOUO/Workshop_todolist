7fa09fc8e927d0f31a346ae7c2c41a3a1c04b1cd
dante <dantecherng@hotmail.com.tw>
2020-12-08 13:41:57 +0800

    move_checked

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,11 @@
 // TO DO
+document.addEventListener('DOMContentLoaded', function() {
+    const checkedlist = document.querySelectorAll('li')
+        // console.log(checkedlist)
+    for (let i = 0; i < checkedlist.length; i++) {
+        checkedlist[i].addEventListener('click', function() {
+            checkedlist[i].classList.add('checked')
+        })
+    }
+
+})
