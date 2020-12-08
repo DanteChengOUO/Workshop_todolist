6055fd24e39bd255d192d76922a1b1856ce27b8b
dante <dantecherng@hotmail.com.tw>
2020-12-08 16:32:24 +0800

    finish

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,5 +1,37 @@
 // TO DO
 document.addEventListener('DOMContentLoaded', function() {
+
+    document.getElementById('addBtn').addEventListener('click', function() {
+        const li = document.createElement('li')
+        const addtext = document.getElementById('input').value
+        li.innerHTML = addtext + '<span class="close">x</span>'
+        document.querySelector('ul').appendChild(li)
+
+
+
+
+
+        const checkedlist = document.querySelectorAll('li')
+        for (let i = 0; i < checkedlist.length; i++) {
+            checkedlist[i].addEventListener('click', function() {
+                checkedlist[i].classList.add('checked')
+            })
+        }
+
+
+        const closelist = document.querySelectorAll('li span')
+        for (let n = 0; n < closelist.length; n++) {
+            closelist[n].addEventListener('click', function() {
+                closelist[n].parentElement.remove()
+            })
+        }
+
+
+
+
+    })
+
+
     const checkedlist = document.querySelectorAll('li')
     for (let i = 0; i < checkedlist.length; i++) {
         checkedlist[i].addEventListener('click', function() {
@@ -14,8 +46,5 @@ document.addEventListener('DOMContentLoaded', function() {
             closelist[n].parentElement.remove()
         })
     }
-
-
-
 
 })
